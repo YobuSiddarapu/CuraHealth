064cb4daa5577b240c3ac5afa53e58be79369137
Yobu <Yobu@DESKTOP-MCPSJPS>
2023-10-11 12:08:14 +0530

    login functionality

--- Empirehomecrm/src/test/java/com/testcases/Logintest.java
+++ Empirehomecrm/src/test/java/com/testcases/Logintest.java
@@ -1,6 +1,7 @@
 package com.testcases;
 
 import org.testng.Assert;
+import org.testng.AssertJUnit;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -9,9 +10,10 @@ import org.testng.annotations.Test;
 import com.base.Basetest;
 import com.pageobjects.Loginfunctionality;
 
+
 public class Logintest extends Basetest {
 
-	Loginfunctionality lf;
+	Loginfunctionality Login;
 
 	public Logintest() {
 		super();   //invokes the parent class constructor
@@ -22,19 +24,21 @@ public class Logintest extends Basetest {
 
 		initialization();
 
-		lf = new Loginfunctionality();
-
-	}
-
-	@Test
-
-	public void loginvalidation() {
-		lf.verifylogin();
-		String urltest = driver.getCurrentUrl();
-		Assert.assertEquals(urltest, "http://empirehome.myprojectsonline.co.in/EmpireHome/Dashboard");
+		Login =new Loginfunctionality();
 		
 
 	}
+@Test
+public void Loginvarification() {
+	Login.verifylogin();
+	String urltest = driver.getCurrentUrl();
+	AssertJUnit.assertEquals(urltest, "http://empirehome.myprojectsonline.co.in/EmpireHome/Dashboard");
+	
+	
+	
+}
+
+	
 
 	@AfterMethod
 	public void teardown() {
